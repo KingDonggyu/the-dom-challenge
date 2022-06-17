c6fc7f97b67156d5a25d3bf38eef922317a33973
KingDonggyu <dgkim69166916@gmail.com>
2022-06-17 20:57:58 +0900

    challenge star-rating

--- star-rating/script.js
+++ star-rating/script.js
@@ -5,5 +5,52 @@
  * @param callback Returns selected star count to callback
  */
 function Star(el, count, callback) {
-    
+  this.$target = document.querySelector(el);
+  this.count = count;
+  this.callback = callback;
+  this.active = -1;
+  this.render();
+  this.bindEvents();
 }
+
+Star.prototype.render = function () {
+  const $fragment = document.createDocumentFragment();
+  for (let i = 0; i < this.count; i++) {
+    const $star = document.createElement('div');
+    $star.classList.add('fa');
+    $star.classList.add('fa-star-o');
+    $star.dataset.ratingId = i;
+    $fragment.appendChild($star);
+  }
+  this.$target.appendChild($fragment);
+};
+
+Star.prototype.bindEvents = function () {
+  this.$target.addEventListener('mouseover', this.onMouseOver.bind(this));
+  this.$target.addEventListener('mouseleave', this.onMouseLeave.bind(this));
+  this.$target.addEventListener('click', this.onClick.bind(this));
+};
+
+Star.prototype.fill = function (ratingId) {
+  for (let i = 0; i < this.count; i++) {
+    if (i <= ratingId) {
+      this.$target.children[i].classList.add('fa-star');
+      continue;
+    }
+    this.$target.children[i].classList.remove('fa-star');
+  }
+};
+
+Star.prototype.onMouseOver = function (e) {
+  const ratingId = e.target.dataset.ratingId;
+  this.fill(ratingId);
+};
+
+Star.prototype.onMouseLeave = function () {
+  this.fill(this.active);
+};
+
+Star.prototype.onClick = function (e) {
+  this.active = e.target.dataset.ratingId;
+  this.callback(this.active + 1);
+};
